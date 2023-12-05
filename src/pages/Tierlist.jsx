205eddda15626cbdbd9a67c1e848a47feffef8ca
Tadeo <tadeoabbruzzese@gmail.com>
2023-12-05 20:55:40 -0300

    añado tierlist

--- src/pages/Tierlist.jsx
+++ src/pages/Tierlist.jsx
@@ -1,47 +1,38 @@
 import styled from "styled-components";
+import TierlistComponent from "../components/Tierlist/TierlistComponent";
 export function Tierlist(props) {
     
     return(
         <Container>
-        <div className="container-global">
-            <div className="simulacra-container">
-                <h1 className="simulacra-tittle">{props.tittle}</h1>
-                 <p className="simulacra-detail"> {props.p}</p>
+            <div className="container-global">
+                <div className="simulacra-container">
+                    <h1 className="simulacra-tittle">{props.tittle}</h1>
+                    <p className="simulacra-detail"> {props.p}</p>
+                </div>
             </div>
-        </div>
-        
+
+        <TierlistComponent />
     </Container>
     );
 }
 const Container =styled.div`
-    height: 100vh;
-    width: 100%
+min-height: 100vh;
+width: 100%;
 
-    .container-global{
-        width: 80%;
-        height: inherit;
-        
-        
-    }
 
-    .simulacra-container{
-        height: 30vh;
-        
-    }
+.container-global{
+    width: 100%;
+    height: inherit;
+    
+    
+}
 
-    .simulacra-tittle{
-        display: flex;
-        justify-content: center;
-        padding: 20px;
-    }
 
-    .simulacra-detail{
-        
-        display: flex;
-        justify-content: center;
-        font-size: 20px;
-        
-        
+.simulacra-tittle{
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+}
 
-    }
+
 `
